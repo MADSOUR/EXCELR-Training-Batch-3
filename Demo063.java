073d010b801f5ceb501f2af9385daf6fe2bc4dba
MADSOUR <hemanthramkumar2000@gmail.com>
2025-02-12 12:31:14 +0530

    Calculating the Sum of First and Last Elements of an Array

---
+++ Demo063.java
@@ -0,0 +1,12 @@
+public class Demo063 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50}; // Sample array
+
+        if (array.length > 0) {
+            int sum = array[0] + array[array.length - 1];
+            System.out.println("Sum of 1st and last element: " + sum);
+        } else {
+            System.out.println("Array is empty.");
+        }
+    }
+}
